6d78c1272282735ce4329caf4a041b83e7c1b757
IlkaMatsimnbe <ilkamatsimbe6@gmail.com>
2026-05-20 12:31:23 +0200

    Separate into smaller components

---
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -0,0 +1,34 @@
+import axios from 'axios';
+import { Header } from '../../components/Header';
+import './HomePage.css';
+import { useEffect, useState} from 'react';
+import { ProductsGrid } from './ProductsGrid';  
+
+
+export function HomePage({cart}) {
+  const [products, setProducts] = useState([]);
+
+
+  useEffect(() => {
+    axios.get('/api/products')
+      .then((response) => {
+        setProducts(response.data);
+      });
+
+  }, []);
+
+
+  return (
+    <>
+
+      <Header cart={cart} />
+
+      <title>Ecommerce Project</title>
+
+
+      <div className="home-page">
+        <ProductsGrid products={products} />
+      </div>
+    </>
+  );
+};
